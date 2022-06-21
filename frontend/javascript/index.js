4f2633658f90fe769f734c8cd46e3542946e1879
Hartmut Bischoff <topofocus@gmail.com>
2022-06-21 11:18:06 +0200

    including of fontsawesome via yarn

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,6 +1,6 @@
 import "index.css"
 import "syntax-highlighting.css"
-
+import "@fortawesome/fontawesome-free/css/all"
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
 
